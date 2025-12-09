9b665ceb4c876162a66352e0b04c3bfab443d5d4
StadnikJu <stadnikjulia14@icloud.com>
2025-12-09 20:43:53 +0100

    completed hw5

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './s1-main/App'
 import reportWebVitals from './reportWebVitals'
 import store from './s2-homeworks/hw10/bll/store'
